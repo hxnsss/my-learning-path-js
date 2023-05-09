d09ad78d289a5afa12beb21c9ce6271a3d972db4
Hxnsss <hxnsss.dev@gmail.com>
2023-05-08 21:36:43 -0400

    agregando operadores de comparacion

--- basics/comparadores.js
+++ basics/comparadores.js
@@ -1,17 +1,39 @@
-/* Comparadores booleanos con (==, !=, ===, !==, <, >, <=, >=) */
+/* Comparadores de comparacion con (==, !=, ===, !==, <, >, <=, >=) */
 
-console.log();
+// Comparador mayor < y menor que >
+console.log(5 < 3);
+// true
+console.log(5 > 3);
+// false
 
-console.log();
+// Comparadores de igualdad estricta === y no estricta ==
+console.log(5 == "5"); // no estricta
+// true
+console.log(5 === "5"); // estricta
+// false
 
-console.log();
+/* Es recomendado no usar el operador de igualdad "no estricto", ya que nos puede causar
+ confusión en ciertos casos especificos. No significa que no debemos utilizarlo, pero si
+ tener en cuenta EN QUE lo utilizamos.*/
 
-console.log();
+// Comparador distinto que "no estricto" != y comparador es distinto que !== (estricto)
+console.log(5 != 3);
+// true
+console.log(5 != "5");
+// false
+console.log(5 !== "5");
+// true
 
-console.log();
+/* En este caso sucede lo mismo que en nuestro anterior caso,
+el comparador es distinto que, estricto y no estricto, deben usarse en casos especificos, ya que pueden causar
+un error o un resultado NO ESPERADO en nuestro codigo. Ya dependiendo de su uso pueden dar resultados diferentes */
 
-console.log();
-
-console.log();
-
-console.log();
+// Comparador mayor o igual que <= y menor o igual que >=
+console.log(5 <= 4);
+// true
+console.log(5 >= 5);
+// true
+console.log(5 <= 6);
+// false
+console.log(5 >= 6);
+// true
