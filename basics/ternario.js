6418e555f6883a53efab106e53f833a3d582eb4b
Hxnsss <hxnsss.dev@gmail.com>
2023-05-10 03:32:16 -0400

    agregando ternarios

---
+++ basics/ternario.js
@@ -0,0 +1,16 @@
+/* Operador ternario (condition ? resultTrue : resultFalse) para elejir uno de dos valores, basandose en un tercer valor.
+ Este nos puede servir como un if simplificado, el cual se puede utilizar para evaluar una condicion
+ que nos puede devolver un valor true o false. */
+
+// Syntaxis:
+// condition ? resultTrue : resultFalse
+
+console.log(5 > 3 ? true : false);
+
+/* 
+Esta misma condicion que tenemos aqui, se leeria de la siguiente manera...
+Si x es mayor que y imprimira true
+Si x es menor que y primira false
+
+Aqui nos podemos dar cuenta que un ternario puede tener casi el mismo uso que un condicional "if"
+*/
